71b1bb58592c1bd71f93ed6e898fdc34985e4a3d
benoel <ibnu.a.azis@gmail.com>
2016-11-16 11:34:41 +0700

    upload db

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $user = "root";
 $pass = "";
-$name = "tokobaju";
+$name = "garis_mode";
 
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
